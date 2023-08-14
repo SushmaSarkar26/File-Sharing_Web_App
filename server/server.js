96e0edd8dddc1e7e9651c95cc67efaee76608576
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2023-08-14 19:54:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,18 +3,32 @@ import cors from 'cors';
 import router from './routes/routes.js';
 import DBConnection from './database/db.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
 dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.use('/', router);
+
+
+app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+    
 
 const PORT = process.env.PORT || 8000;
 
 DBConnection();
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT);
